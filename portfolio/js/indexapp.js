72179d7f754561c3d742adcd37622ebecebf2b4b
Rishita Srivastava <66817759+Rishita11@users.noreply.github.com>
2022-05-30 12:24:17 +0530

    Delete portfolio directory

--- portfolio/js/indexapp.js
+++
@@ -1,5 +0,0 @@
-const navbarToggler = document.querySelector(".navbar-toggler");
-const collapseNav = document.querySelector("#navbarNav");
-navbarToggler.addEventListener("click", () => {
-    collapseNav.classList.toggle("show");
-});
